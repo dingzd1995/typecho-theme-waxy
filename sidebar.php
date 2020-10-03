747389ef1b3f4934ffb10d2a306c8e9b308df6bc
dingzd <dd8989089a4@foxmail.com>
2020-10-03 22:14:56 +0800

    修复点击搜索框小放大镜图标无法跳转搜索结果页的问题

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,9 @@
     <form id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
         <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
         <input aria-label="search input" type="text" name="s" class="text asearch" placeholder="<?php _e('输入关键字搜索'); ?>" />
-        <i  title="搜索" class="glyphicon glyphicon-search" id="icon-search" style="position: absolute;top: 30%;right: 2%;color: #a6a6a6;margin: 1px 5px;"></i>
+        <a href="javascript:void(0)" onclick="document.getElementById('search').submit();">
+	     <i title="搜索" class="glyphicon glyphicon-search" id="icon-search" style="position: absolute;top: 30%;right: 2%;color: #a6a6a6;margin: 1px 5px;"></i>
+	</a>
     </form>
   </div>
 </div>
