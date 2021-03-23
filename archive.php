cf912765144a5947d68d83d71abb83e752d9d3f0
dingzd1995 <dingzd1995@gmail.com>
2021-03-23 22:49:11 +0800

    修复分类和搜索页面摘要模式显示不正常的问题

--- archive.php
+++ archive.php
@@ -21,44 +21,92 @@
             </div>
 
             <?php while($this->next()): ?>
+			<!----全文模式开始----->
+			<?php if ($this->options->articles_list==1):?>
+			<article id="<?php $this->cid() ?>" class="post">
 
-            <article id="<?php $this->cid() ?>" class="post">
-            
-                <?php if (array_key_exists('star',unserialize($this->___fields()))): ?><div class="featured" title="推荐文章">
-                    <i class="glyphicon glyphicon-star"></i>
-                </div><?php endif; ?>
-            
-                <div class="post-head">
-                    <h1 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
-                    <div class="post-meta">
-                        <span class="author">作者：<a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a></span>
-                        <time class="post-date" datetime="<?php $this->date('c'); ?>" title="<?php $this->date('Y年m月d日'); ?>">时间：<?php $this->date('Y年m月d日'); ?></time>
-                        <span class="author">分类：<?php $this->category(','); ?></span>
-                        <span class="author">字数：<?php echo art_count($this->cid); ?></span>
-                    </div>
-                    <div class="post-border"></div>
-                </div>
-                <?php if (array_key_exists('img',unserialize($this->___fields()))): ?>
+				<?php if (array_key_exists('star',unserialize($this->___fields()))): ?><div class="featured" title="推荐文章">
+					<i class="glyphicon glyphicon-star"></i>
+				</div><?php endif; ?>
+
+				<div class="post-head">
+					<h1 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
+					<div class="post-meta">
+						<span class="author">作者：<a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a></span>
+						<time class="post-date" datetime="<?php $this->date('c'); ?>" title="<?php $this->date('Y年m月d日'); ?>">时间：<?php $this->date('Y年m月d日'); ?></time>
+						<span class="author">分类：<?php $this->category(','); ?></span>
+						<span class="author">字数：<?php echo art_count($this->cid); ?></span>
+						<!--span class="author">阅读：<?php //get_post_view($this) ?></span-->
+					</div>
+					<div class="post-border"></div>
+				</div>
+						
+				<?php if (array_key_exists('img',unserialize($this->___fields()))): ?>
                 <div class="featured-media">
                     <a href="<?php $this->permalink() ?>"><img src="<?php $this->fields->img(); ?>" alt="<?php $this->title() ?>"></a>
                 </div>
                 <?php endif; ?>
-                <div class="post-content">
-                    <!--?php $this->content(); ?-->
-                    <?php echo getIndexContent($this->content,$this->permalink); ?>
-                </div>
-                <footer class="post-footer clearfix">
-                    <div class="pull-left tag-list">
-                        <i class="glyphicon glyphicon-folder-open"></i>
-                        <?php $this->tags(' , ', true, 'none'); ?>
-                    </div>
-                    <div class="pull-right post-permalink" >
-                    	<a href="<?php $this->permalink() ?>#comments" class="btn btn-default">前往评论</a>
-            		</div>
-                </footer>
-            </article>
+				
+				<div class="post-content">
+					<?php echo getIndexContent($this->content,$this->permalink); ?>
+				</div>
+				
 
-            <?php endwhile; ?>
+				<footer class="post-footer clearfix">
+					
+					<div class="pull-left tag-list" >
+						<i class="glyphicon glyphicon-folder-open"></i>
+						<?php $this->tags(' , ', true, 'none'); ?>
+					</div>
+					
+					<div class="pull-right post-permalink" >
+						<a href="<?php $this->permalink() ?>#comments" class="btn btn-default">前往评论</a>
+					</div>
+				</footer>
+			</article>
+			<?php endif; ?>
+			<!----摘要模式开始----->
+			<?php if ($this->options->articles_list==0):?>
+
+			<article id="<?php $this->cid() ?>" class="post" style="padding:25px 10px;">
+				
+				<?php if (array_key_exists('star',unserialize($this->___fields()))): ?>
+					<div class="featured" title="推荐文章"> <i class="glyphicon glyphicon-star"></i></div>
+				<?php endif; ?>
+				<div class="excerpt">
+				<?php if (array_key_exists('img',unserialize($this->___fields()))): ?>
+					<div class="excerpt-img">
+						<img class="lazyload" src="'<?php $this->options->JQlazyload_gif(); ?>" data-original="<?php $this->fields->img(); ?>" alt="<?php $this->title() ?>" title="<?php $this->title() ?>">
+					</div>
+				<?php else: ?>
+					<?php if (getFirstImg($this->content)): ?>
+					<div class="excerpt-img">
+						<img class="lazyload" src="<?php $this->options->JQlazyload_gif(); ?>" data-original="<?php echo getFirstImg($this->content); ?>" alt="<?php $this->title() ?>" title="<?php $this->title() ?>">
+					</div>
+					<?php endif; ?>
+				<?php endif; ?>
+				<div class="post-excerpt">
+					
+					<div class="excerpt-title">
+						<a href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+					</div>
+					<div class="excerpt-info">
+						<div class="excerpt-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span><a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a></div> 
+						<div class="excerpt-item"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span><?php $this->date('Y-m-d'); ?></div> 
+						<div class="excerpt-item"><span class="glyphicon glyphicon-tag" aria-hidden="true"></span><?php $this->category(','); ?></div>
+						<!--div class="excerpt-item"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><?php $this->tags(' , ', true, 'none'); ?></div-->
+					</div>
+					<div class="excerpt-content">
+						<?php if (array_key_exists('info',unserialize($this->___fields()))){ $this->fields->info();} else { echo getExcerpt($this->text,75,'');} ?>
+						<a href="<?php $this->permalink() ?>" style="white-space:nowrap;" > - 阅读更多 - </a>
+					</div>
+				</div>
+				</div>
+			</article>
+
+			<?php endif; ?>
+
+			<?php endwhile; ?>
 
             <nav class="pagination" role="navigation">
                     
